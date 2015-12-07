be6f2bdca61281e441a5951d8800f09752061aa2
PhillHenry <phillhenry@gmail.com>
2015-12-07 11:57:55 +0000

    Now has all instruments LMAX offers.

--- spark/src/main/scala/com/henryp/lambda/spark/streaming/lmax/MarketDataReceiver.scala
+++ spark/src/main/scala/com/henryp/lambda/spark/streaming/lmax/MarketDataReceiver.scala
@@ -1,10 +1,12 @@
 package com.henryp.lambda.spark.streaming.lmax
 
+import java.util
+
 import com.henryp.lambda.logging.Logging
 import com.henryp.lambda.spark.streaming.lmax.MarketDataReceiver.loginClient
 import com.lmax.api.account.LoginRequest.ProductType
 import com.lmax.api.account.{LoginCallback, LoginRequest}
-import com.lmax.api.orderbook.{OrderBookEvent, OrderBookEventListener, OrderBookSubscriptionRequest}
+import com.lmax.api.orderbook._
 import com.lmax.api.{Callback, FailureResponse, LmaxApi, Session}
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.receiver.Receiver
@@ -41,16 +43,52 @@ object MarketDataReceiver {
     }
 
     override def onLoginSuccess(session: Session): Unit = {
-      info(s"Successfully ogged in to LMAX ($url) as $username")
-      session.registerOrderBookEventListener(this) // see com.lmax.api.MarketDataClient
+      info(s"Successfully logged in to LMAX ($url) as $username")
 
-      session.subscribe(new OrderBookSubscriptionRequest(4001), new Callback { // TODO, more instruments (4001 = EUR/USD)
-        override def onFailure(failureResponse: FailureResponse): Unit = error(failureResponse.getDescription)
+      subscribeToAll(session)
 
-        override def onSuccess(): Unit = info("Subscribed!")
-      })
       session.start()
     }
+
+    def subscriptionCallback(instrumentId: Long): Callback = new Callback {
+      override def onFailure(failureResponse: FailureResponse): Unit = error(failureResponse.getDescription)
+
+      override def onSuccess(): Unit = info(s"Subscribed to $instrumentId")
+    }
+
+    /**
+      * @see com.lmax.api.MarketDataClient
+      */
+    def subscribeToAll(session: Session): Unit = {
+      import scala.collection.JavaConversions._
+
+      session.registerOrderBookEventListener(this)
+
+      val offset  = Array(0L)
+      val hasMore = Array(true)
+
+      while (hasMore(0)) {
+        val searchInstrumentCallback = new SearchInstrumentCallback() {
+          override def onSuccess(instruments: util.List[Instrument], hasMoreResults: Boolean): Unit = {
+            hasMore(0) = hasMoreResults
+
+            for (instrument <- instruments) {
+              val instrumentId = instrument.getId
+              info(s"Instrument: $instrumentId,  + ${instrument.getName}")
+              session.subscribe(new OrderBookSubscriptionRequest(instrumentId), subscriptionCallback(instrumentId))
+              offset(0) = instrumentId
+            }
+          }
+
+          override def onFailure(failureResponse: FailureResponse): Unit = {
+            hasMore(0) = false
+            throw new RuntimeException("Failed: " + failureResponse)
+          }
+        }
+        session.searchInstruments(new SearchInstrumentRequest("", offset(0)), searchInstrumentCallback)
+      }
+    }
+
 
     override def notify(orderBookEvent: OrderBookEvent): Unit = {
       info("order book event = " + orderBookEvent.toString)
